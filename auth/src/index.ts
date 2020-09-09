af6a74dbe67561cc0e652ec89501e3926b771b0a
vastlaan <vastlaan@mail.com>
2020-09-09 19:17:57 +0200

    Move creating app to separate file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,5 @@
-import express from "express";
 import mongoose from "mongoose";
-import { json } from "body-parser";
-import "express-async-errors"; //handles asynchronous errors as syncronous
-import cookieSession from "cookie-session";
-import { CurrentUserRouter } from "./routes/current-user";
-import { SignupRouter } from "./routes/signup";
-import { SigninRouter } from "./routes/signin";
-import { SignoutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-// express trust nginx proxy
-app.set("trust proxy", true);
-
-app.use(json());
-app.use(
-    cookieSession({
-        // turn out the encryption of the cookie (JWT is already encrypted) to simplify reading it accross multiple services running different languages
-        signed: false,
-        // require https connection to use the cookie
-        secure: true,
-    })
-);
-
-app.use(CurrentUserRouter);
-app.use(SigninRouter);
-app.use(SignoutRouter);
-app.use(SignupRouter);
-
-// app.all listens to all type of http requests; thanks to express-async-errors we don't have to handle async errors ourselves
-app.all("*", async () => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -48,6 +13,7 @@ const start = async () => {
             useUnifiedTopology: true,
             useCreateIndex: true,
         });
+        console.log("Connected to MongoDB");
     } catch (err) {
         console.error(err);
     }
