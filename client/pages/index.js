b93efb0b30718469ff62aa63920fbd2189ae2d93
vastlaan <vastlaan@mail.com>
2020-09-17 22:14:38 +0200

    Create Client app

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function landing() {
+    return <Container>Landing</Container>;
+}
+
+const Container = styled.div`
+    font-size: 2rem;
+    font-family: "Verdana", sans-serif;
+    color: grey;
+`;
